214ba74f2cc4558fa2a259d4361e7a8ea979c786
David Lai <laidav@gmail.com>
2023-07-10 14:25:22 -0400

    fix typings

--- packages/rxjs-state/src/Operators/ofType.ts
+++ packages/rxjs-state/src/Operators/ofType.ts
@@ -2,9 +2,7 @@ import { OperatorFunction } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { Action } from '../Models/Action';
 
-export const ofType: (
-  type: string,
-) => OperatorFunction<Action<unknown>, Action<unknown>> =
+export const ofType: (type: string) => OperatorFunction<Action, Action> =
   (type: string) => (dispatcher$) => {
     return dispatcher$.pipe(filter((action) => action?.type === type));
   };
