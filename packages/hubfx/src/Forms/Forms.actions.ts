c0c4027e8fefdf47ae7c1bab5fa94f595d5d6bd4
David Lai <laidav@gmail.com>
2023-08-01 11:37:36 -0400

    add async validation form forms control change

--- packages/hubfx/src/Forms/Forms.actions.ts
+++ packages/hubfx/src/Forms/Forms.actions.ts
@@ -1,13 +1,54 @@
+import { map, filter } from 'rxjs/operators';
 import { Action } from '../Models/Action';
 import { ControlChange, ControlAsyncValidationResponse } from './Models/Forms';
+import { getFormControl } from './FormsReducer.reducer';
+import { Effect } from '../Models/Effect';
+import { FormErrors } from './Models/Forms';
+import { ofType } from '../Operators/ofType';
 
 export const FORMS_CONTROL_CHANGE = 'FORMS_CONTROL_CHANGE';
 export const controlChange = <T, S>(
-  payload: ControlChange<T, S>,
-): Action<ControlChange<T, S>> => ({
-  type: FORMS_CONTROL_CHANGE,
-  payload,
-});
+  controlChange: ControlChange<T, S>,
+): Action<ControlChange<T, S>> => {
+  const { state, controlRef } = controlChange;
+  const { config } = getFormControl(controlRef, state);
+  const { asyncValidators } = config;
+
+  let scopedEffects: Effect<unknown, FormErrors>[];
+
+  if (asyncValidators && asyncValidators.length) {
+    scopedEffects = asyncValidators.reduce((acc, validator) => {
+      const effect: Effect<unknown, FormErrors> = (actions$) => {
+        return actions$
+          .pipe(
+            ofType(FORMS_CONTROL_CHANGE),
+            filter(
+              (action: Action<ControlChange<T, S>>) =>
+                action.payload.controlRef === controlRef,
+            ),
+            map((action) => {
+              return action.payload.value;
+            }),
+          )
+          .pipe(
+            validator,
+            map((errors) =>
+              asyncValidationResponseSuccess({ controlRef, errors }),
+            ),
+          );
+      };
+
+      return acc.concat(effect);
+    }, []);
+  }
+
+  return {
+    type: FORMS_CONTROL_CHANGE,
+    key: controlChange.controlRef.join(':'),
+    payload: controlChange,
+    scopedEffects,
+  };
+};
 
 export const FORMS_CONTROL_ASYNC_VALIDATION_RESPONSE_SUCCESS =
   'FORMS_CONTROL_ASYNC_VALIDATION_RESPONSE_SUCCESS';
