68d9aa6e9d7c84f82afa7d05a23215575d6bb270
David Lai <laidav@gmail.com>
2023-08-25 09:51:48 -0400

    reorganize reducer tests

--- packages/hubfx/src/Forms/Forms.reducer.test.ts
+++ packages/hubfx/src/Forms/Forms.reducer.test.ts
@@ -1099,110 +1099,6 @@ describe('handleAsyncValidationResponseSuccess', () => {
   });
 });
 
-describe('buildFormsReducer', () => {
-  const initialState = buildControlState(config) as FormGroup<Contact>;
-
-  it('should react to FORMS_CONTROL_CHANGE for a FC -> FG', () => {
-    expect(
-      formsReducer(initialState, {
-        type: FORMS_CONTROL_CHANGE,
-        payload: {
-          value: 'Homer',
-          controlRef: ['firstName'],
-        },
-      }),
-    ).toEqual({
-      ...initialState,
-      errors: {
-        firstNameNotSameAsLast: false,
-      },
-      dirty: true,
-      value: {
-        firstName: 'Homer',
-        lastName: '',
-        email: '',
-        phone: '',
-        emergencyContacts: [],
-        doctorInfo: {
-          firstName: '',
-          lastName: '',
-          email: '',
-        },
-      },
-      controls: {
-        ...initialState.controls,
-        firstName: {
-          ...initialState.controls.firstName,
-          controlRef: ['firstName'],
-          value: 'Homer',
-          dirty: true,
-          touched: false,
-          valid: true,
-          errors: {
-            required: false,
-          },
-          asyncValidateInProgress: {},
-          validating: false,
-        },
-      },
-    });
-  });
-
-  it('should react to a FORMS_CONTROL_CHANGE FG -> FA -> FG', () => {
-    const clonedConfig: FormGroupConfig = cloneDeep(config);
-    (<FormArrayConfig>(
-      clonedConfig.formGroupControls.emergencyContacts
-    )).formArrayControls = emergencyContactConfigs;
-
-    const initialState = buildControlState(clonedConfig) as FormGroup<Contact>;
-
-    const newValue = {
-      firstName: 'Moe changed',
-      lastName: 'Syzlak changed',
-      email: 'moe@changed.com',
-      relation: 'friend changed',
-    };
-    const expectedNewState = cloneDeep(initialState);
-
-    expectedNewState.value.emergencyContacts[1] = newValue;
-    expectedNewState.dirty = true;
-    expectedNewState.controls.emergencyContacts.value = [
-      initialState.controls.emergencyContacts.value[0],
-      newValue,
-    ];
-    expectedNewState.controls.emergencyContacts.dirty = true;
-    expectedNewState.controls.emergencyContacts.controls[1].value = newValue;
-    expectedNewState.controls.emergencyContacts.controls[1].dirty = true;
-
-    expectedNewState.controls.emergencyContacts.controls[1].controls.firstName.value =
-      newValue.firstName;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.firstName.dirty =
-      true;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.lastName.value =
-      newValue.lastName;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.lastName.dirty =
-      true;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.email.value =
-      newValue.email;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.email.dirty =
-      true;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.relation.value =
-      newValue.relation;
-    expectedNewState.controls.emergencyContacts.controls[1].controls.relation.dirty =
-      true;
-
-    const newState = formsReducer(initialState, {
-      type: FORMS_CONTROL_CHANGE,
-      payload: {
-        value: newValue,
-        controlRef: ['emergencyContacts', 1],
-      },
-    });
-
-    expect(newState).toEqual(expectedNewState);
-  });
-});
-
 describe('getChildControls', () => {
   it('should get child controls', () => {
     const clonedConfig: FormGroupConfig = cloneDeep(config);
@@ -1234,63 +1130,174 @@ describe('getChildControls', () => {
   });
 });
 
-describe('resetControl', () => {
-  let clonedConfig: FormGroupConfig;
-  let initialState: FormGroup<Contact>;
-  const newValue = {
-    firstName: 'Moe changed',
-    lastName: 'Syzlak changed',
-    email: 'moe@changed.com',
-    relation: 'friend changed',
-  };
+describe('formsReducer', () => {
+  describe('reacting to change control', () => {
+    const initialState = buildControlState(config) as FormGroup<Contact>;
 
-  beforeEach(() => {
-    clonedConfig = cloneDeep(config);
-    (<FormArrayConfig>(
-      clonedConfig.formGroupControls.emergencyContacts
-    )).formArrayControls = emergencyContactConfigs;
+    it('should react to FORMS_CONTROL_CHANGE for a FC -> FG', () => {
+      expect(
+        formsReducer(initialState, {
+          type: FORMS_CONTROL_CHANGE,
+          payload: {
+            value: 'Homer',
+            controlRef: ['firstName'],
+          },
+        }),
+      ).toEqual({
+        ...initialState,
+        errors: {
+          firstNameNotSameAsLast: false,
+        },
+        dirty: true,
+        value: {
+          firstName: 'Homer',
+          lastName: '',
+          email: '',
+          phone: '',
+          emergencyContacts: [],
+          doctorInfo: {
+            firstName: '',
+            lastName: '',
+            email: '',
+          },
+        },
+        controls: {
+          ...initialState.controls,
+          firstName: {
+            ...initialState.controls.firstName,
+            controlRef: ['firstName'],
+            value: 'Homer',
+            dirty: true,
+            touched: false,
+            valid: true,
+            errors: {
+              required: false,
+            },
+            asyncValidateInProgress: {},
+            validating: false,
+          },
+        },
+      });
+    });
 
-    initialState = buildControlState(clonedConfig) as FormGroup<Contact>;
+    it('should react to a FORMS_CONTROL_CHANGE FG -> FA -> FG', () => {
+      const clonedConfig: FormGroupConfig = cloneDeep(config);
+      (<FormArrayConfig>(
+        clonedConfig.formGroupControls.emergencyContacts
+      )).formArrayControls = emergencyContactConfigs;
+
+      const initialState = buildControlState(
+        clonedConfig,
+      ) as FormGroup<Contact>;
+
+      const newValue = {
+        firstName: 'Moe changed',
+        lastName: 'Syzlak changed',
+        email: 'moe@changed.com',
+        relation: 'friend changed',
+      };
+      const expectedNewState = cloneDeep(initialState);
+
+      expectedNewState.value.emergencyContacts[1] = newValue;
+      expectedNewState.dirty = true;
+      expectedNewState.controls.emergencyContacts.value = [
+        initialState.controls.emergencyContacts.value[0],
+        newValue,
+      ];
+      expectedNewState.controls.emergencyContacts.dirty = true;
+      expectedNewState.controls.emergencyContacts.controls[1].value = newValue;
+      expectedNewState.controls.emergencyContacts.controls[1].dirty = true;
+
+      expectedNewState.controls.emergencyContacts.controls[1].controls.firstName.value =
+        newValue.firstName;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.firstName.dirty =
+        true;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.lastName.value =
+        newValue.lastName;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.lastName.dirty =
+        true;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.email.value =
+        newValue.email;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.email.dirty =
+        true;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.relation.value =
+        newValue.relation;
+      expectedNewState.controls.emergencyContacts.controls[1].controls.relation.dirty =
+        true;
+
+      const newState = formsReducer(initialState, {
+        type: FORMS_CONTROL_CHANGE,
+        payload: {
+          value: newValue,
+          controlRef: ['emergencyContacts', 1],
+        },
+      });
+
+      expect(newState).toEqual(expectedNewState);
+    });
   });
 
-  it('should reset entire form', () => {
-    const changedState = formsReducer(initialState, {
-      type: FORMS_CONTROL_CHANGE,
-      payload: {
-        value: newValue,
-        controlRef: ['emergencyContacts', 1],
-      },
-    });
-    const stateReset = formsReducer(changedState, {
-      type: FORMS_RESET_CONTROL,
-      payload: [],
+  describe('when resettting form', () => {
+    let clonedConfig: FormGroupConfig;
+    let initialState: FormGroup<Contact>;
+    const newValue = {
+      firstName: 'Moe changed',
+      lastName: 'Syzlak changed',
+      email: 'moe@changed.com',
+      relation: 'friend changed',
+    };
+
+    beforeEach(() => {
+      clonedConfig = cloneDeep(config);
+      (<FormArrayConfig>(
+        clonedConfig.formGroupControls.emergencyContacts
+      )).formArrayControls = emergencyContactConfigs;
+
+      initialState = buildControlState(clonedConfig) as FormGroup<Contact>;
     });
 
-    expect(stateReset).toEqual(initialState);
-  });
+    it('should reset entire form', () => {
+      const changedState = formsReducer(initialState, {
+        type: FORMS_CONTROL_CHANGE,
+        payload: {
+          value: newValue,
+          controlRef: ['emergencyContacts', 1],
+        },
+      });
+      const stateReset = formsReducer(changedState, {
+        type: FORMS_RESET_CONTROL,
+        payload: [],
+      });
 
-  it('should reset a control and update ancestor values', () => {
-    const changedState = formsReducer(initialState, {
-      type: FORMS_CONTROL_CHANGE,
-      payload: {
-        value: newValue,
-        controlRef: ['emergencyContacts', 1],
-      },
+      expect(stateReset).toEqual(initialState);
     });
-    const stateReset = formsReducer(changedState, {
-      type: FORMS_RESET_CONTROL,
-      payload: ['emergencyContacts', 1],
+
+    it('should reset a control and update ancestor values', () => {
+      const changedState = formsReducer(initialState, {
+        type: FORMS_CONTROL_CHANGE,
+        payload: {
+          value: newValue,
+          controlRef: ['emergencyContacts', 1],
+        },
+      });
+      const stateReset = formsReducer(changedState, {
+        type: FORMS_RESET_CONTROL,
+        payload: ['emergencyContacts', 1],
+      });
+      const expectedState = cloneDeep(changedState) as FormGroup<Contact>;
+      expectedState.dirty = false;
+      expectedState.value = initialState.value;
+      expectedState.controls.emergencyContacts.value =
+        initialState.controls.emergencyContacts.value;
+      expectedState.controls.emergencyContacts.dirty = false;
+
+      (<FormArray<unknown>>(
+        expectedState.controls.emergencyContacts
+      )).controls[1] = (<FormArray<unknown>>(
+        initialState.controls.emergencyContacts
+      )).controls[1];
+
+      expect(stateReset).toEqual(expectedState);
     });
-    const expectedState = cloneDeep(changedState) as FormGroup<Contact>;
-    expectedState.dirty = false;
-    expectedState.value = initialState.value;
-    expectedState.controls.emergencyContacts.value =
-      initialState.controls.emergencyContacts.value;
-    expectedState.controls.emergencyContacts.dirty = false;
-
-    (<FormArray<unknown>>expectedState.controls.emergencyContacts).controls[1] =
-      (<FormArray<unknown>>initialState.controls.emergencyContacts).controls[1];
-
-    expect(stateReset).toEqual(expectedState);
   });
 });
