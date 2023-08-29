f964c0e36150aac321098acc85fe352ee1aacaca
David Lai <laidav@gmail.com>
2023-08-28 20:24:10 -0400

    lint fixes in async validator

--- packages/hubfx/src/Forms/AsyncValidators.ts
+++ packages/hubfx/src/Forms/AsyncValidators.ts
@@ -1,42 +1,32 @@
-import { of, delay, mergeMap, tap } from 'rxjs';
+import { of, delay, mergeMap } from 'rxjs';
 import { ValidatorAsyncFn } from './Models/Forms';
 
 export const uniqueEmail: ValidatorAsyncFn = (control$) => {
-  // return of({ uniqueEmail: true }).pipe(delay(1000));
   return control$.pipe(
-    mergeMap((control) => of({ uniqueEmail: true }).pipe(delay(250))),
+    mergeMap(() => of({ uniqueEmail: true }).pipe(delay(250))),
   );
 };
 
 export const blacklistedEmail: ValidatorAsyncFn = (control$) => {
-  // return of({ uniqueEmail: true }).pipe(delay(1000));
   return control$.pipe(
-    mergeMap((control) => of({ blacklistedEmail: true }).pipe(delay(300))),
+    mergeMap(() => of({ blacklistedEmail: true }).pipe(delay(300))),
   );
 };
 
 export const arrayLengthError: ValidatorAsyncFn = (control$) => {
   return control$.pipe(
-    mergeMap((control) => of({ arrayLengthError: true }).pipe(delay(350))),
+    mergeMap(() => of({ arrayLengthError: true }).pipe(delay(350))),
   );
 };
 
 export const uniqueFirstAndLastName: ValidatorAsyncFn = (control$) => {
   return control$.pipe(
-    // tap((control) => console.log(control, 'in validator')),
-    mergeMap((control) =>
-      of({ uniqueFirstAndLastName: true }).pipe(delay(400)),
-    ),
+    mergeMap(() => of({ uniqueFirstAndLastName: true }).pipe(delay(400))),
   );
 };
 
 export const blacklistedDoctorType: ValidatorAsyncFn = (control$) => {
   return control$.pipe(
-    // tap((control) => console.log(control, 'in validator')),
-    mergeMap((control) => of({ blacklistedDoctorType: true }).pipe(delay(500))),
+    mergeMap(() => of({ blacklistedDoctorType: true }).pipe(delay(500))),
   );
 };
-
-//Change payload with the state
-//map control to validator
-// then send action with controlrRef
