a4e368f563ce67ef061ea895c116bd54df29fa68
David Lai <laidav@gmail.com>
2023-09-03 19:05:49 -0400

    udpate form component

--- packages/react-forms/src/Components/Form.tsx
+++ packages/react-forms/src/Components/Form.tsx
@@ -1,21 +1,22 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import { HubFactory, Hub } from '@hubfx/core';
-import { buildReducer } from '@hubfx/forms';
+import { buildReducer, AbstractControl } from '@hubfx/forms';
 import { AbstractControlConfig } from '@hubfx/forms';
 import { useObservable } from '../Hooks/useObservable';
 
 const FormContext = React.createContext(null);
 
-interface FormProps extends PropsWithChildren {
+interface FormProps {
   formConfig: AbstractControlConfig;
   hub: Hub;
+  children: (state: AbstractControl<unknown>) => React.ReactNode;
 }
 
 const Form = ({ formConfig, hub = HubFactory(), children }: FormProps) => {
   const state = useObservable(hub.store({ reducer: buildReducer(formConfig) }));
   return (
     <FormContext.Provider value={{ state, dispatch: hub.dispatch }}>
-      {children}
+      {children(state)}
     </FormContext.Provider>
   );
 };
