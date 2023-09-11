0c5240b30398d35fad16dd50952cee1bf3fd97fd
David Lai <laidav@gmail.com>
2023-09-11 14:25:25 -0400

    create formarray component

--- packages/react-forms/src/Components/FormArray.tsx
+++ packages/react-forms/src/Components/FormArray.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { ControlRef } from '@hubfx/forms';
+import React, { useContext } from 'react';
+import {
+  AbstractControl,
+  ControlRef,
+  FormArray as IFormArray,
+  getControl,
+} from '@hubfx/forms';
+import { Form, FormContext } from './Form';
+
+export interface FormArrayChildrenProps {
+  controls: AbstractControl<unknown>[];
+}
 
 export interface FormArrayProps {
   component: React.JSXElementConstructor<unknown>;
   controlRef: ControlRef;
+  children?: (props: FormArrayChildrenProps) => React.ReactNode;
 }
 
-export const FormArray = ({ controlRef }) => {
-  return <div></div>;
+export const FormArray = ({ controlRef, children }: FormArrayProps) => {
+  const { state } = useContext(FormContext);
+  const { controls } = getControl(controlRef, state) as IFormArray<unknown>;
+  return <div>{children && children({ controls })}</div>;
 };
